d8a26ae6c298a76736355ec1dc6a4c2e7b5e061a
Adam <yellows.afterlife@gmail.com>
2024-04-26 10:13:05 -0700

    Basic world gen

--- src/game.cpp
+++ src/game.cpp
@@ -32,7 +32,6 @@
 #define STB_IMAGE_IMPLEMENTATION   
 
 #include "stb_image.h"
-#include "SimplexNoise.h"
 
 Game game;
 
@@ -68,7 +67,7 @@ static const char* fragment_shader_source =
     "uniform sampler2D ourTexture;\n"
     "uniform vec4 our_color;\n"
     "void main() {\n"
-    "    finalColor = texture(ourTexture, TexCoord) * vec4(vertex_color.x, our_color.y, vertex_color.z, 1.0f);\n"
+    "    finalColor = texture(ourTexture, TexCoord) * vec4(our_color.x, our_color.y, our_color.z, 1.0f);\n"
     "}\n";
 
 unsigned int shader_program(const char *vertex_shader_source, const char *fragment_shader_source) 
@@ -153,6 +152,74 @@ Sprite make_sprite(const char* filename)
     stbi_image_free(data);
 
     return temp;
+}
+
+//NOISE FUNCTION
+static int SEED = 0;
+
+static int hash[] = {208,34,231,213,32,248,233,56,161,78,24,140,71,48,140,254,245,255,247,247,40,
+                     185,248,251,245,28,124,204,204,76,36,1,107,28,234,163,202,224,245,128,167,204,
+                     9,92,217,54,239,174,173,102,193,189,190,121,100,108,167,44,43,77,180,204,8,81,
+                     70,223,11,38,24,254,210,210,177,32,81,195,243,125,8,169,112,32,97,53,195,13,
+                     203,9,47,104,125,117,114,124,165,203,181,235,193,206,70,180,174,0,167,181,41,
+                     164,30,116,127,198,245,146,87,224,149,206,57,4,192,210,65,210,129,240,178,105,
+                     228,108,245,148,140,40,35,195,38,58,65,207,215,253,65,85,208,76,62,3,237,55,89,
+                     232,50,217,64,244,157,199,121,252,90,17,212,203,149,152,140,187,234,177,73,174,
+                     193,100,192,143,97,53,145,135,19,103,13,90,135,151,199,91,239,247,33,39,145,
+                     101,120,99,3,186,86,99,41,237,203,111,79,220,135,158,42,30,154,120,67,87,167,
+                     135,176,183,191,253,115,184,21,233,58,129,233,142,39,128,211,118,137,139,255,
+                     114,20,218,113,154,27,127,246,250,1,8,198,250,209,92,222,173,21,88,102,219};
+
+int noise2(int x, int y)
+{
+    int tmp = hash[(y + SEED) % 256];
+    return hash[(tmp + x) % 256];
+}
+
+float lin_inter(float x, float y, float s)
+{
+    return x + s * (y-x);
+}
+
+float smooth_inter(float x, float y, float s)
+{
+    return lin_inter(x, y, s * s * (3-2*s));
+}
+
+float noise2d(float x, float y)
+{
+    int x_int = x;
+    int y_int = y;
+    float x_frac = x - x_int;
+    float y_frac = y - y_int;
+    int s = noise2(x_int, y_int);
+    int t = noise2(x_int+1, y_int);
+    int u = noise2(x_int, y_int+1);
+    int v = noise2(x_int+1, y_int+1);
+    float low = smooth_inter(s, t, x_frac);
+    float high = smooth_inter(u, v, x_frac);
+    return smooth_inter(low, high, y_frac);
+}
+
+float perlin2d(float x, float y, float freq, int depth)
+{
+    float xa = x*freq;
+    float ya = y*freq;
+    float amp = 1.0;
+    float fin = 0;
+    float div = 0.0;
+
+    int i;
+    for(i=0; i<depth; i++)
+    {
+        div += 256 * amp;
+        fin += noise2d(xa, ya) * amp;
+        amp /= 2;
+        xa *= 2;
+        ya *= 2;
+    }
+
+    return fin/div;
 }
 
 int main()
@@ -257,7 +324,9 @@ int main()
 
     // glEnableVertexAttribArray(0);
 
-    Sprite chunk_sprite = make_sprite("magma.png");
+    Sprite ltt_sprite = make_sprite("grass.png");
+    Sprite gtt_sprite = make_sprite("grass2.png");
+    Sprite gss_sprite = make_sprite("ground3.png");
 
     while(game.window.running)
     {
@@ -314,10 +383,33 @@ int main()
             int shift_location = glGetUniformLocation(program, "shift");
             glUniform2f(shift_location, chunk_space_x, chunk_space_y);
 
-            glBindTexture(GL_TEXTURE_2D, chunk_sprite.texture);
+            float noise_input_x = current_chunk->index.x;
+            float noise_input_y = current_chunk->index.y;
 
+            float noise = perlin2d(noise_input_x, noise_input_y, 0.1, 4);
+
+            if(noise > 0.33)
+            {
+                glBindTexture(GL_TEXTURE_2D, gtt_sprite.texture);
+            }
+            else
+            {
+                glBindTexture(GL_TEXTURE_2D, ltt_sprite.texture);
+            }
+            if(noise > 0.66)
+            {
+                glBindTexture(GL_TEXTURE_2D, gss_sprite.texture);
+            }
+
+            int vertex_color_location = glGetUniformLocation(program, "our_color");
+            glUniform4f(vertex_color_location, 1.0, 1.0, 1.0, 1.0f);
+            
             glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)0);
         }
+
+        float time = SDL_GetTicks() * 0.001;
+
+        float green_value = (sin(time)) / 2.0f + 0.5f;
 
         //entity loop
         for(int i = 0; i < max_entity_count; i++)
@@ -352,18 +444,14 @@ int main()
                 int scale_location = glGetUniformLocation(program, "scale");
                 glUniform2f(scale_location, entity_scale_x, entity_scale_y);
 
+                int vertex_color_location = glGetUniformLocation(program, "our_color");
+                glUniform4f(vertex_color_location, 0.41, green_value, 0.53, 1.0f);
+
                 glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)0);
 
                 glBlendFunc(GL_ONE, GL_ZERO);
             }
         }
-
-        float time = SDL_GetTicks() * 0.001;
-
-        float green_value = (sin(time)) / 2.0f + 0.5f;
-
-        int vertex_color_location = glGetUniformLocation(program, "our_color");
-        glUniform4f(vertex_color_location, 0.41, green_value, 0.53, 1.0f);
 
         SDL_GL_SwapWindow(game.window.window);
     
